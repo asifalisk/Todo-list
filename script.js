1429587b40fbfb50f504f056216caa6d0836a5c8
Asif <asifakki9100@gmail.com>
2023-04-06 10:50:22 +0530

    status change updated

--- script.js
+++ script.js
@@ -20,10 +20,30 @@ saveButtonEle.onclick=function (){
 }
 
 
-function onTodoStatusChange(checklistId,labelId){
+function onTodoStatusChange(checklistId,labelId,TodoId){
     let checkboxElement=document.getElementById(checklistId)
     let labelElement=document.getElementById(labelId);
-    labelElement.classList.toggle("checked")
+    labelElement.classList.toggle("checked");
+    
+    let TodofindIndex=Todolist.findIndex(function(eachTodo){
+        let eachTodoId="todo"+eachTodo.uniqueNo;
+        if(eachTodoId === TodoId){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+
+
+    let TodoObject=Todolist[TodofindIndex];
+
+    if(TodoObject.isChecked === true){
+        TodoObject.isChecked = false;
+    }
+    else{
+        TodoObject.isChecked = true;
+    }
 }
 
 function deleteContainer(TodoId){
@@ -31,7 +51,7 @@ function deleteContainer(TodoId){
     containerItem.removeChild(lielement);
     let findIndex=Todolist.findIndex(function(eachTodo){
         let eachTodoId="todo"+eachTodo.uniqueNo;
-        if(eachTodoId===TodoId){
+        if(eachTodoId === TodoId){
             return true;
         }
         else{
@@ -62,8 +82,10 @@ function createAndAppendChild(Todo) {
     let inputEle = document.createElement("input");
     inputEle.type = "checkbox";
     inputEle.id = checklistId;
+    inputEle.checked=Todo.isChecked;
+    
     inputEle.onclick=function (){
-        onTodoStatusChange(checklistId,labelId);
+        onTodoStatusChange(checklistId,labelId,TodoId);
     }
     liEle.appendChild(inputEle);
 
@@ -75,6 +97,9 @@ function createAndAppendChild(Todo) {
     labelEle.setAttribute("for", checklistId);
     labelEle.textContent = Todo.text;
     labelEle.id=labelId
+    if(Todo.isChecked===true){
+        labelEle.classList.add("checked");
+    }
     labelContainer.appendChild(labelEle);
 
     let deleteItem = document.createElement("div");
@@ -96,9 +121,15 @@ function addTodoItem(){
     let inputTextElement=document.getElementById("inputText");
     let inputTextValue=inputTextElement.value;
 
+    if(inputTextValue=== ""){
+        alert("Enter Valid Text")
+        return;
+    }
+
     newTodo={
         text:inputTextValue,
-        uniqueNo:TodoCount
+        uniqueNo:TodoCount,
+        isChecked:false
     }
     Todolist.push(newTodo);
     createAndAppendChild(newTodo);
